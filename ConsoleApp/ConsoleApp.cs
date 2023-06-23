5400bf69d922214263425d4a578abfaed18560b5
Charles Mendes <charlesluizmendes@gmail.com>
2023-06-23 18:30:46 -0300

    Criando Repositorio

---
+++ ConsoleApp/ConsoleApp.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp
+{
+    public class ConsoleApp : IHostedService
+    {
+        private readonly IHostApplicationLifetime _appLifeTime;
+        private readonly ILogger<ConsoleApp> _logger;
+
+        public ConsoleApp(IHostApplicationLifetime appLifeTime,
+            ILogger<ConsoleApp> logger)
+        {
+            _appLifeTime = appLifeTime;
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _appLifeTime.ApplicationStarted.Register(() =>
+            {
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        _logger.LogInformation("Hello World!");
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+
+                    finally
+                    {
+                        _appLifeTime.StopApplication();
+                    }
+                });
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp
+{
+    static class Program
+    {
+        public static IConfigurationRoot Configuration { get; private set; }
+
+        static async Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
+
+            Console.ReadKey();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+          Host.CreateDefaultBuilder(args)
+           .ConfigureAppConfiguration((hostBuilderContext, configurationBuilder) =>
+           {
+               var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+               configurationBuilder
+                   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                   .AddJsonFile($"appsettings.{environment}.json", true, true)
+                   .AddEnvironmentVariables();
+
+               var configurationRoot = configurationBuilder.Build();
+               Configuration = configurationRoot;
+
+           }).ConfigureLogging((logging) =>
+           {
+               logging.AddSimpleConsole(o =>
+               {
+                   o.SingleLine = true;
+               });
+
+           }).ConfigureServices((services) =>
+           {
+               services.AddHostedService<ConsoleApp>();
+           });
+    }
+}
